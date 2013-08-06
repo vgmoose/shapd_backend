1e11e51c103b3d563b994166526270a7e45e4971
VGMoose <vgmoose@gmail.com>
2013-08-06 17:34:51 -0400

    updated things

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ Shapd::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.compress = true
 
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -40,9 +40,9 @@ Shapd::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = false
+  config.force_ssl = true
     
-config.assets.enabled = true
+  config.assets.enabled = true
 
 
   # Set to :debug to see everything in the log.
